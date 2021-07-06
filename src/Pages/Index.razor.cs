e3d1ec34404c11d61f82a13231bf5480b234145e
Edi Wang <Edi.Wang@outlook.com>
2021-07-06 13:26:55 +0800

    CIDR struct

---
+++ src/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CIDRCalc.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IJSRuntime JavaScriptRuntime { get; set; }
+    }
+}
