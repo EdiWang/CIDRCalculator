8d549473bc31b21ec3df68cccbe3832943c5faf2
Edi Wang <Edi.Wang@outlook.com>
2021-10-25 13:08:30 +0800

    upgrade to .NET 6 RC2

--- src/Pages/Index.razor.cs
+++ src/Pages/Index.razor.cs
@@ -1,70 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 
-namespace CIDRCalc.Pages
+namespace CIDRCalc.Pages;
+
+public partial class Index
 {
-    public partial class Index
+    [Inject]
+    public IJSRuntime JavaScriptRuntime { get; set; }
+
+    public IPRange2CIDRModel IPRange2CIDRModel { get; set; }
+
+    public CIDR2IPRangeModel CIDR2IPRangeModel { get; set; }
+
+    public IEnumerable<CIDR> CIDRs { get; set; }
+
+    public string IPRange { get; set; }
+
+    public Index()
     {
-        [Inject]
-        public IJSRuntime JavaScriptRuntime { get; set; }
-
-        public IPRange2CIDRModel IPRange2CIDRModel { get; set; }
-
-        public CIDR2IPRangeModel CIDR2IPRangeModel { get; set; }
-
-        public IEnumerable<CIDR> CIDRs { get; set; }
-
-        public string IPRange { get; set; }
-
-        public Index()
-        {
-            IPRange2CIDRModel = new();
-            CIDR2IPRangeModel = new();
-            CIDRs = Array.Empty<CIDR>();
-        }
-
-        private void GetCIDRs()
-        {
-            var startAddress = IPAddress.Parse(IPRange2CIDRModel.StartIP);
-            var endAddress = IPAddress.Parse(IPRange2CIDRModel.EndIP);
-
-            CIDRs = CIDR.Split(startAddress, endAddress);
-        }
-
-        private void GetIPRange()
-        {
-            var inputCIDR = CIDR2IPRangeModel.CIDR.Split('/');
-            if (inputCIDR.Length > 1)
-            {
-                var fromCidr = new CIDR(IPAddress.Parse(inputCIDR[0]), uint.Parse(inputCIDR[1]));
-                IPRange = $"{fromCidr.NetworkAddress} - {fromCidr.LastAddress}";
-            }
-            else
-            {
-                IPRange = inputCIDR[0];
-            }
-        }
+        IPRange2CIDRModel = new();
+        CIDR2IPRangeModel = new();
+        CIDRs = Array.Empty<CIDR>();
     }
 
-    public class IPRange2CIDRModel
+    private void GetCIDRs()
     {
-        [Required]
-        [RegularExpression(@"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ErrorMessage = "Please input a valid IPv4 Address")]
-        public string StartIP { get; set; }
+        var startAddress = IPAddress.Parse(IPRange2CIDRModel.StartIP);
+        var endAddress = IPAddress.Parse(IPRange2CIDRModel.EndIP);
 
-        [Required]
-        [RegularExpression(@"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ErrorMessage = "Please input a valid IPv4 Address")]
-        public string EndIP { get; set; }
+        CIDRs = CIDR.Split(startAddress, endAddress);
     }
 
-    public class CIDR2IPRangeModel
+    private void GetIPRange()
     {
-        [Required]
-        [RegularExpression(@"^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$", ErrorMessage = "Please input a vlid IPv4 CIDR")]
-        public string CIDR { get; set; }
+        var inputCIDR = CIDR2IPRangeModel.CIDR.Split('/');
+        if (inputCIDR.Length > 1)
+        {
+            var fromCidr = new CIDR(IPAddress.Parse(inputCIDR[0]), uint.Parse(inputCIDR[1]));
+            IPRange = $"{fromCidr.NetworkAddress} - {fromCidr.LastAddress}";
+        }
+        else
+        {
+            IPRange = inputCIDR[0];
+        }
     }
+}
+
+public class IPRange2CIDRModel
+{
+    [Required]
+    [RegularExpression(@"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ErrorMessage = "Please input a valid IPv4 Address")]
+    public string StartIP { get; set; }
+
+    [Required]
+    [RegularExpression(@"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ErrorMessage = "Please input a valid IPv4 Address")]
+    public string EndIP { get; set; }
+}
+
+public class CIDR2IPRangeModel
+{
+    [Required]
+    [RegularExpression(@"^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$", ErrorMessage = "Please input a vlid IPv4 CIDR")]
+    public string CIDR { get; set; }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,23 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
-namespace CIDRCalc
+namespace CIDRCalc;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
     }
 }
