dfbd187b7eb9fc051b9cd654a8e24b57ed61ea53
Edi Wang <Edi.Wang@outlook.com>
2023-04-13 14:17:01 +0800

    ipv6 from CIDR

--- src/Pages/Index.razor.cs
+++ src/Pages/Index.razor.cs
@@ -16,7 +16,9 @@ public partial class Index
 
     public IEnumerable<CIDR> CIDRs { get; set; }
 
-    public string IPRange { get; set; }
+    public string IPRangeStart { get; set; }
+
+    public string IPRangeEnd { get; set; }
 
     public Index()
     {
@@ -33,17 +35,19 @@ public partial class Index
         CIDRs = CIDR.Split(startAddress, endAddress);
     }
 
-    private void GetIPRangev4()
+    private void GetIPRange()
     {
         var inputCIDR = CIDR2IPRangeModel.CIDR.Split('/');
         if (inputCIDR.Length > 1)
         {
             var fromCidr = new CIDR(IPAddress.Parse(inputCIDR[0]), uint.Parse(inputCIDR[1]));
-            IPRange = $"{fromCidr.NetworkAddress} - {fromCidr.LastAddress}";
+            IPRangeStart = fromCidr.NetworkAddress.ToString();
+            IPRangeEnd = fromCidr.LastAddress.ToString();
         }
         else
         {
-            IPRange = inputCIDR[0];
+            IPRangeStart = inputCIDR[0];
+            IPRangeEnd = inputCIDR[0];
         }
     }
 }
@@ -62,6 +66,6 @@ public class IPRange2CIDRModel
 public class CIDR2IPRangeModel
 {
     [Required]
-    [RegularExpression(@"^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$", ErrorMessage = "Please input a vlid IPv4 CIDR")]
+    [RegularExpression(@"^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$|^s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:)))(%.+)?s*(\/([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-8]))?$", ErrorMessage = "Please input a valid IPv4 / IPv6 CIDR")]
     public string CIDR { get; set; }
 }
