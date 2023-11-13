0b87b8610fcb1b69d94ce0a60b3c86ddf7f8c2cf
Edi Wang <Edi.Wang@outlook.com>
2023-11-13 15:00:50 +0800

    upgrade Microsoft.FluentUI.AspNetCore.Components

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,10 @@
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.FluentUI.AspNetCore.Components;
 using CIDRCalc.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddFluentUIComponents(options =>
-{
-    options.HostingModel = BlazorHostingModel.WebAssembly;
-});
 
 await builder.Build().RunAsync();
