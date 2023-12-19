2b87bde6f9c978b0d282195fbf10c789fce8d997
Edi Wang <Edi.Wang@outlook.com>
2023-12-19 11:46:28 +0800

    fix

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,12 @@
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CIDRCalc.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
